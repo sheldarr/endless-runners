6bb28a8a67ce21bb1859942ae1ab0d7bfd540cb0
Bartek <bartosz.baczek@gmail.com>
2016-02-22 19:06:34 +0100

    renamed backgroundLoader to boardLoader

---
+++ src/boardLoader.js
@@ -0,0 +1,16 @@
+import PIXI from 'pixi.js';
+
+const backgroundFactory = {
+    create () {
+        var background = new PIXI.Sprite(PIXI.utils.TextureCache['./assets/tiles/dirt.png']);
+        background.position.x = 0;
+        background.position.y = 0;
+
+        background.scale.x = 1;
+        background.scale.y = 1;
+
+        return background;
+    }
+};
+
+export default backgroundFactory;
