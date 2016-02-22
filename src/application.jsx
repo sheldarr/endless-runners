0bdee5b17575ef9ac4d6f83aa11dac77b7ecaf92
sheldar@wp.pl <sheldar@wp.pl>
2016-02-22 19:33:16 +0100

    Options are now connected with menu component

--- src/application.jsx
+++ src/application.jsx
@@ -2,13 +2,20 @@ import React from 'react';
 
 import assetsLoader from './assetsLoader';
 import Menu from './menu.jsx';
+import Options from './options.jsx';
 import Scene from './scene.jsx';
+
+const ACTIVE_DISPLAY = {
+    MENU: 0,
+    GAME: 1,
+    OPTIONS: 2
+};
 
 const Application = React.createClass({
     getInitialState () {
         return {
+            activeDisplay: ACTIVE_DISPLAY.MENU,
             gameLoaded: false,
-            gameStarted: false,
             loadingProgress: 0,
             loadingResource: ''
         };
@@ -30,22 +37,46 @@ const Application = React.createClass({
 
     startGame () {
         this.setState({
-            gameStarted: true
+            activeDisplay: ACTIVE_DISPLAY.GAME
         });
+    },
+
+    displayOptions () {
+        this.setState({
+            activeDisplay: ACTIVE_DISPLAY.OPTIONS
+        });
+    },
+
+    displayMenu () {
+        this.setState({
+            activeDisplay: ACTIVE_DISPLAY.Menu
+        });
+    },
+
+    renderActiveDisplay () {
+        if (this.state.activeDisplay === ACTIVE_DISPLAY.GAME) {
+            return <Scene/>;
+        }
+
+        if (this.state.activeDisplay === ACTIVE_DISPLAY.OPTIONS) {
+            return <Options onClose={this.displayMenu}/>;
+        }
+
+        return (
+            <Menu
+                gameLoaded={this.state.gameLoaded}
+                loadingProgress={this.state.loadingProgress}
+                loadingResource={this.state.loadingResource}
+                optionsSelected={this.displayOptions}
+                startGameSelected={this.startGame}
+            />
+        );
     },
 
     render () {
         return (
             <div>
-                {this.state.gameStarted
-                    ? <Scene/>
-                    : <Menu
-                        gameLoaded={this.state.gameLoaded}
-                        loadingProgress={this.state.loadingProgress}
-                        loadingResource={this.state.loadingResource}
-                        onStartGame={this.startGame}
-                      />
-                }
+                {this.renderActiveDisplay()}
             </div>
         );
     }
