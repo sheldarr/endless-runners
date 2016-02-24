87966fe520c91c6b8daa42a289e24afc4a4773f3
sheldar@wp.pl <sheldar@wp.pl>
2016-02-24 21:08:48 +0100

    Extracted methods for vertical and horizontal collisions

--- src/collisionHandler.js
+++ src/collisionHandler.js
@@ -1,4 +1,18 @@
 const collisionHandler = {
+    handleVerticalCollision(player, tile) {
+        if ((player.position.y < tile.position.y && player.position.y + player.height < tile.position.y) ||
+        (player.position.y > tile.position.y + tile.height && player.position.y + player.height > tile.position.y + tile.height)) {
+            player.position.dy *= -1.1;
+        }
+    },
+
+    handleHorizontalCollision(player, tile) {
+        if ((player.position.x < tile.position.x && player.position.x + player.width < tile.position.x) ||
+        (player.position.x > tile.position.x + tile.width && player.position.x + player.width > tile.position.x + tile.width)) {
+            player.position.dx *= -1.1;
+        }
+    },
+
     handle (player, tiles, callback) {
         tiles.forEach((tile) => {
             if (tile.isSolid) {
@@ -6,16 +20,8 @@ const collisionHandler = {
                 player.position.x + player.width + player.position.dx < tile.position.x ||
                 player.position.y + player.position.dy > tile.position.y + tile.height ||
                 player.position.y + player.height + player.position.dy < tile.position.y)) {
-                    //  vertical collision
-                    if ((player.position.y < tile.position.y && player.position.y + player.height < tile.position.y) ||
-                    (player.position.y > tile.position.y + tile.height && player.position.y + player.height > tile.position.y + tile.height)) {
-                        player.position.dy *= -1.1;
-                    }
-                    //  horizontal collision
-                    if ((player.position.x < tile.position.x && player.position.x + player.width < tile.position.x) ||
-                    (player.position.x > tile.position.x + tile.width && player.position.x + player.width > tile.position.x + tile.width)) {
-                        player.position.dx *= -1.1;
-                    }
+                    this.handleVerticalCollision(player, tile);
+                    this.handleHorizontalCollision(player, tile)
                 }
             }
         });
