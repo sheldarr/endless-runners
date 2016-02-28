848c42ff545a6d84099a187d8b1c21a582c87fef
Bartek <bartosz.baczek@gmail.com>
2016-02-28 12:04:14 +0100

    Added camera factory

---
+++ src/factories/cameraFactory.js
@@ -0,0 +1,14 @@
+import PIXI from 'pixi.js';
+
+const cameraFactory = {
+    create () {
+        var camera = {
+            xPosition: 0,
+            width: 0
+        };
+        camera.rectangle = new PIXI.Rectangle(0, 0, 640, 400);
+        return camera;
+    }
+};
+
+export default cameraFactory;
