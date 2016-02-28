3d2159f1ffb2ce75a88cd7abd4d47ec6b70d80f9
sheldar@wp.pl <sheldar@wp.pl>
2016-02-28 16:01:07 +0100

    playersLobby now shows different player sprites

--- src/components/playersLobby.jsx
+++ src/components/playersLobby.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button, Col, Glyphicon, Grid, Row } from 'react-bootstrap';
+import { Button, Col, Glyphicon, Grid, Image, Row } from 'react-bootstrap';
 
 const playersLobby = React.createClass({
     propTypes: {
@@ -7,9 +7,70 @@ const playersLobby = React.createClass({
         onStart: React.PropTypes.func.isRequired
     },
 
+    getInitialState () {
+        return {
+            characters: [
+                'blob',
+                'dragon',
+                'ettercap',
+                'goblin',
+                'mage',
+                'player',
+                'skeleton',
+                'spider',
+                'tortoise',
+                'wasp'
+            ],
+            selectedCharacter: 'player'
+        };
+    },
+
+    previousCharacter () {
+        let selectedCharacterIndex = this.state.characters.indexOf(this.state.selectedCharacter);
+
+        if (selectedCharacterIndex === 0) {
+            this.setState({
+                selectedCharacter: this.state.characters[this.state.characters.length - 1]
+            });
+
+            return;
+        }
+
+        this.setState({
+            selectedCharacter: this.state.characters[selectedCharacterIndex - 1]
+        });
+    },
+
+    nextCharacter () {
+        let selectedCharacterIndex = this.state.characters.indexOf(this.state.selectedCharacter);
+
+        if (selectedCharacterIndex === this.state.characters.length - 1) {
+            this.setState({
+                selectedCharacter: this.state.characters[0]
+            });
+
+            return;
+        }
+
+        this.setState({
+            selectedCharacter: this.state.characters[selectedCharacterIndex + 1]
+        });
+    },
+
     render () {
         return (
             <Grid>
+                <Row>
+                    <Col xs={4} xsOffset={4}>
+                        <Button bsStyle="primary" onClick={this.previousCharacter}>
+                            <Glyphicon glyph="arrow-left"/>
+                        </Button>
+                        <Image rounded src={`./assets/characters/${this.state.selectedCharacter}.png`}/>
+                        <Button bsStyle="primary" onClick={this.nextCharacter}>
+                            <Glyphicon glyph="arrow-right"/>
+                        </Button>
+                    </Col>
+                </Row>
                 <Row>
                     <Col style={{marginTop: 10}} xs={4} xsOffset={4}>
                         <Button block bsStyle="success" onClick={this.props.onStart}>
