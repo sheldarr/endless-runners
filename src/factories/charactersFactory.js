2852ca48abac11227191f5926d3511f0cd778148
sheldar@wp.pl <sheldar@wp.pl>
2016-02-29 17:36:11 +0100

    Removed setDefaultProperties method

--- src/factories/charactersFactory.js
+++ src/factories/charactersFactory.js
@@ -1,11 +1,5 @@
 import PIXI from 'pixi.js';
 const charactersFactory = {
-    setDefaultProperties (character, position) {
-        character.worldPosition = position;
-        character.worldPosition.dx = 0;
-        character.worldPosition.dy = 0;
-    },
-
     createCharacter (characterOptions, position) {
         let character = new PIXI.Sprite(PIXI.utils.TextureCache[`./assets/characters/${characterOptions.asset}`]);
 
