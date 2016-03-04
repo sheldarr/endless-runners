5f13c830e411bd27fdf06d7670816b3aff61632a
sheldar@wp.pl <sheldar@wp.pl>
2016-03-04 18:21:02 +0100

    Removed wrong character in eslintrc.json file

--- src/factories/cameraFactory.js
+++ src/factories/cameraFactory.js
@@ -6,7 +6,7 @@ const cameraFactory = {
             xPosition: 0,
             width: 0
         };
-        camera.rectangle = new PIXI.Rectangle(0, 0, 640, 400);
+        camera.rectangle = new PIXI.Rectangle(0, 0, 640, 400);;
         return camera;
     }
 };
