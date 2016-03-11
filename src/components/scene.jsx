94b46afe30650fe2644a3a468a3a00653d029a4f
Bartek <bartosz.baczek@gmail.com>
2016-03-11 23:14:42 +0100

    Removed background from scene state

--- src/components/scene.jsx
+++ src/components/scene.jsx
@@ -1,7 +1,6 @@
 import PIXI from 'pixi.js';
 import React from 'react';
 
-import backgroundFactory from '../factories/backgroundFactory.js';
 import cameraHandler from '../handlers/cameraHandler.js';
 import collisionHandler from '../handlers/collisionHandler.js';
 import coordinatesConverter from '../handlers/coordinatesConverter.js';
@@ -15,7 +14,6 @@ const Scene = React.createClass({
 
     getInitialState () {
         return {
-            background: undefined,
             renderer: new PIXI.WebGLRenderer(640, 320),
             stage: new PIXI.Container()
         };
@@ -27,20 +25,15 @@ const Scene = React.createClass({
     },
 
     componentDidMount () {
-        const background = backgroundFactory.createSewerBackground();
-
-        this.state.stage.addChild(background);
+        this.state.stage.addChild(this.props.gameState.board.sprites);
 
         this.props.gameState.players.forEach((player) => {
             this.state.stage.addChild(player.sprite);
         });
+
         document.getElementById('scene').appendChild(this.state.renderer.view);
 
-        this.setState({
-            background
-        }, () => {
-            this.animate();
-        });
+        this.animate();
 
         window.addEventListener('resize', (event) => {
             rendererHandler.adjustToWindow(event.target.innerWidth, event.target.innerHeight,
@@ -52,8 +45,8 @@ const Scene = React.createClass({
         requestAnimationFrame(this.animate);
 
         playersControlsHandler.handle(this.props.gameState.keyboard, this.props.gameState.players);
-        collisionHandler.handle(this.props.gameState.players, this.state.background.children);
-        coordinatesConverter.toScreen(this.props.gameState.players, this.state.background.children, this.props.gameState.camera);
+        collisionHandler.handle(this.props.gameState.players, this.props.gameState.board.sprites.children);
+        coordinatesConverter.toScreen(this.props.gameState.players, this.props.gameState.board.sprites.children, this.props.gameState.camera);
         cameraHandler.handle(this.props.gameState.camera);
 
         this.state.renderer.render(this.state.stage);
