d50c1b51f72d964e5e69d807f97ca31ec5045522
Bartek <bartosz.baczek@gmail.com>
2016-03-11 21:57:26 +0100

    Added camera to gameState

--- src/objects/game.js
+++ src/objects/game.js
@@ -1,9 +1,11 @@
+import Camera from './camera.js';
 import Keyboard from './keyboard.js';
 
 function Game () {
     this.players = [];
     this.keyboard = new Keyboard();
     this.gamepads = [];
+    this.camera = new Camera(0);
 }
 
 export default Game;
