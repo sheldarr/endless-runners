f8b2485b0b3abebbc19c619255e28e3a6672f710
Bartek <bartosz.baczek@gmail.com>
2016-03-15 12:20:41 +0100

    Refactored collisionHandler

--- src/handlers/collisionHandler.js
+++ src/handlers/collisionHandler.js
@@ -1,5 +1,35 @@
 const collisionHandler = {
-    handleHorizontalCollision (player, tile) {
+    handle (players, tiles) {
+        players.forEach((player) => {
+            this.handleCollisionWithLeftScreenEdge(player);
+            this.handleCollisionWithObstacles(player, tiles);
+        });
+    },
+
+    handleCollisionWithLeftScreenEdge (player) {
+        if (player.index === 0) {
+            if (!player.wasHit && player.sprite.position.x <= 0) {
+                player.onHitHappened();
+            }
+            console.log('jeszcze zyc:   ' + player.lives);
+        }
+    },
+
+    handleCollisionWithObstacles (player, tiles) {
+        tiles.forEach((tile) => {
+            if (tile.isSolid && !player.ignoreCollisions) {
+                if (!(player.worldPosition.x + player.velocity.x > tile.worldPosition.x + tile.width ||
+                player.worldPosition.x + player.sprite.width + player.velocity.x < tile.worldPosition.x ||
+                player.worldPosition.y + player.velocity.y > tile.worldPosition.y + tile.height ||
+                player.worldPosition.y + player.sprite.height + player.velocity.y < tile.worldPosition.y)) {
+                    this.handleVerticalCollisionWithObstacles(player, tile);
+                    this.handleHorizontalCollisionWithObstacles(player, tile);
+                }
+            }
+        });
+    },
+
+    handleHorizontalCollisionWithObstacles (player, tile) {
         if ((player.worldPosition.y + 0.5 * player.sprite.height < tile.worldPosition.y + 0.5 * tile.height &&
             player.worldPosition.y + player.sprite.height - tile.worldPosition.y < player.worldPosition.x + player.sprite.width - tile.worldPosition.x &&
             player.worldPosition.y + player.sprite.height - tile.worldPosition.y < tile.worldPosition.x + tile.width - player.worldPosition.x) ||
@@ -11,7 +41,7 @@ const collisionHandler = {
         }
     },
 
-    handleVerticalCollision (player, tile) {
+    handleVerticalCollisionWithObstacles (player, tile) {
         if ((player.worldPosition.x + 0.5 * player.sprite.width < tile.worldPosition.x + 0.5 * tile.width &&
             player.worldPosition.x + player.sprite.width - tile.worldPosition.x < player.worldPosition.y + player.sprite.height - tile.worldPosition.y &&
             player.worldPosition.x + player.sprite.width - tile.worldPosition.x < tile.worldPosition.y + tile.height - player.worldPosition.y) ||
@@ -21,32 +51,6 @@ const collisionHandler = {
             player.worldPosition.x -= player.velocity.x;
             player.velocity.x = 0;
         }
-    },
-
-    handleLeftScreenEdgeCollision (player) {
-        if (player.index === 0) {
-            if (player.sprite.position.x <= 0) {
-                player.onHitHappened();
-                console.log('jeszcze zyc:   ' + player.lives);
-            }
-        }
-    },
-
-    handle (players, tiles) {
-        players.forEach((player) => {
-            this.handleLeftScreenEdgeCollision(player);
-            tiles.forEach((tile) => {
-                if (tile.isSolid && !player.ignoreCollisions) {
-                    if (!(player.worldPosition.x + player.velocity.x > tile.worldPosition.x + tile.width ||
-                    player.worldPosition.x + player.sprite.width + player.velocity.x < tile.worldPosition.x ||
-                    player.worldPosition.y + player.velocity.y > tile.worldPosition.y + tile.height ||
-                    player.worldPosition.y + player.sprite.height + player.velocity.y < tile.worldPosition.y)) {
-                        this.handleVerticalCollision(player, tile);
-                        this.handleHorizontalCollision(player, tile);
-                    }
-                }
-            });
-        });
     }
 };
 
