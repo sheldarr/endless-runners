9af152b631f2a743e64ac701d43d279f4936a059
Bartek <bartosz.baczek@gmail.com>
2016-03-15 12:06:12 +0100

    added gameWorld json containing data about world, and tiles size

---
+++ src/constants/gameWorld.js
@@ -0,0 +1,9 @@
+const gameWorld = {
+    tileSizeInPixels: 32,
+    mapHeightInTiles: 12,
+    mapWidthInTiles: 40,
+    mapWidthInPixels: 1280,     // tileSideInPixels * mapWidthInTiles,
+    mapHeightInPixels: 384      // tileSideInPixels * mapHeightInTiles,
+};
+
+export default gameWorld;
